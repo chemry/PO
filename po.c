a7845a78252983e34b9ed627af5482a167151286
chemry <cmr99223@126.com>
2018-03-30 21:59:53 +0800

    Update po.c

--- po.c
+++ po.c
@@ -6,6 +6,15 @@
 #include <string.h>
 char * users[20];
 int i, user_num;
+const char * sch_alg[10] = {"fcfs", "pr", "sjf"};
+const int scheduler_num = 3;
+
+void close_pipe(const int rfd, const int wfd) { // close a pair of file descriptors
+    if (close(rfd) == -1 || close(wfd) == -1) {
+        printf("couldn't close file");
+        exit(EXIT_FAILURE);
+    }
+}
 
 char * format(char * s){
 	if(s[0] >= 97 && s[0] <= 122){
@@ -27,19 +36,29 @@ int _getline(char * s, int lim){
 	@para
 	line: the command in a single line, need to be seperated;
 	cmd: the command represented in integers, seperated.
-		cmd[0]: the command type, 0 for add class, 1 for add meeting, 2 for add gathering, 3 for add batch
+		cmd[0]: the command type, 0 for add class, 1 for add meeting, 2 for add gathering, 3 for add batch, 4 for print schedule, 5 for print report
+		
+		----- for command type 0, 1, 2 -------------
 		cmd[1]: the caller, specified using the index of array users[].
 		cmd[2]: the date, range from 0 - 13, corresponding to Apr 1 to Apr 14.
 		cmd[3]: the start time, range from 0 - 10, corresponding to 08:00 to 18:00
 		cmd[4]: the durationg time, range from 0 - 10.
 		cmd[5]: the number of callees, may be empty when the command is addClass.
 		cmd[6] - cmd[@return]: the calles, specified useing the index of array users[].
+
+		----- for command type 3, 5 -----
+		file: the name of the file to import commands
+
+		----- for command type 4 -----
+		cmd[1]: the user to print the schedule
+		cmd[2]: the type of scheduling algorithm, specified using the index of array sch_alg[].
+		file: the name of the file to be saved.
 	
 	@return
 	return the length of cmd after transformation.
 	return -1 if the command is invalid.
 */
-int sep_line(char * line, int * cmd){
+int sep_line(char * line, int * cmd, char * file){
 	char sep[10][20];
 	memset(sep, 0, sizeof(sep));
 	int cur = 0, p = 0;
@@ -53,13 +72,21 @@ int sep_line(char * line, int * cmd){
 			sep[cur][p++] = c;
 		}
 	}
-	if(cur < 5) return -1; 
+
 	// change the command type to integer
 	if(strcmp(sep[0], "addClass") == 0) cmd[0] = 0;
 	else if(strcmp(sep[0], "addMeeting") == 0) cmd[0] = 1;
 	else if(strcmp(sep[0], "addGathering") == 0) cmd[0] = 2;
 	else if(strcmp(sep[0], "addBatch") == 0) cmd[0] = 3;
+	else if(strcmp(sep[0], "printSchd") == 0) cmd[0] = 4;
+	else if(strcmp(sep[0], "printReport") == 0) cmd[0] = 5;
 	else return -1; // invalid command specifier
+
+	// add batch or print report.
+	if(cmd[0] == 3 || cmd[0] == 5) {
+		strcpy(file, sep[1]);
+		return 1;
+	}
 
 	// change the caller to integer
 	for(i = 0; i < user_num; i++){
@@ -69,6 +96,19 @@ int sep_line(char * line, int * cmd){
 		}
 	}
 	if(i == user_num) return -1; // there are no compitable user name.
+
+	// print schedule
+	if(cmd[0] == 4){
+		for(i = 0; i < scheduler_num; i++){
+			if(strcmp(sep[2], sch_alg[i]) == 0){
+				cmd[2] = i;
+				break;
+			}
+		}
+		if(i == scheduler_num) return -1;
+		strcpy(file, sep[3]);
+		return 3;
+	}
 
 	// change the date to integer
 	if(strncmp(sep[2], "201804", 6) != 0) return -1; // the date is not in 2018/04
@@ -101,15 +141,26 @@ int sep_line(char * line, int * cmd){
 		}
 		if(j == user_num) return -1; // cannot find the corresponding name
 	}
-
 	return 6 + i;
 
 }
 
+void FCFS_scheduler(const int rp_pipe, const int wo_pipe) {
+
+}
+
+void output(const int * rs_pipe, const int rp_pipe){
+
+}
+
+
 int main(int argc, char *argv[]) {
 
-	char line[110];
-	int cmd[15];
+	char line[110], file[100];
+	int cmd[15], n;
+	
+	//int 
+
 
 	// read the user num and user name
 	user_num = argc - 1;
@@ -123,10 +174,60 @@ int main(int argc, char *argv[]) {
 	// print initial message
 	printf("~~WELCOME TO PO~~\n");
 	printf("Please enter ->\n");
-	// make pipes and child scheduler, child output output module.
+
+	/* make pipes
+		p2s: pipe from parent to scheduler process.
+		s2o: pipe from scheduler to output module.
+		p2o: pipe from parent to output module.
+	*/
+	int p2s_fd[scheduler_num][2];
+	int s2o_fd[scheduler_num][2];
+	int p2o_fd[2];
+
+	for(i = 0; i < scheduler_num; i++){
+		if(pipe(p2s_fd[i]) < 0 || pipe(s2o_fd[i]) < 0)
+			perror("pipe create failed");
+	}
+	if(pipe(p2o_fd) < 0) perror("pipe create failed");
+
+	//fork child scheduler, child output output module.
+	int pid;
+
+	// the first FCFS scheduler
+	pid = fork();
+	if(pid == 0){
+		close_pipe(p2s_fd[0][1], s2o_fd[0][0]);
+
+		// the main scheduler function
+		FCFS_scheduler(p2s_fd[0][0], s2o_fd[0][1]);
+		return 0;
+	}
+
+	// fork the output module
+	pid = fork();
+	if(pid == 0){
+		int rp[10];
+		for(i = 0; i < scheduler_num; i++){
+			if(close(s2o_fd[i][1]) == -1) perror("close failed 1");
+			rp[i] = s2o_fd[i][0];
+		}
+		if(close(p2o_fd[1]) == -1) perror("close failed 2");
+
+		// the main function
+		output(rp, p2o_fd[0]);
+		return 0;
+	}
 
 
-	//
+	// parent process
+	// close the read pipe with schedulers.
+	for(i = 0; i < scheduler_num; i++)
+		if(close(p2s_fd[i][0]) == -1) perror("close failed 3");
+	// close the read pipe with output module
+	if(close(p2o_fd[0]) == -1) perror("close failed 4");
+	
+
+	// read from the command line.
 	while(_getline(line, 100)){
 
 		//printf("entered %s\n", line);
@@ -134,9 +235,16 @@ int main(int argc, char *argv[]) {
 			printf("-> Bye!\n");
 			break;
 		} else {
-			int len = sep_line(line, cmd);
-			for(i = 0; i < len; i++){
+			if((n = sep_line(line, cmd, file)) == -1){
+				printf("Invalid Command!\n");
+				break;
+			}
+
+			for(i = 0; i < n; i++){
 				printf("%d ", cmd[i]);
+			}
+			if(cmd[0] >= 3){
+				printf("filename: %s", file);
 			}
 			printf("\n");
 		}
